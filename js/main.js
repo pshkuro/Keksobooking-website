cf9e96313d6462efffaaaa4db9fd06017b999a73
Полина <polina110297@yandex.ru>
2020-01-29 17:35:15 +0300

    Реализовала Добавление и Проверку данных водной функции

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ function createMockAdverts(count) {
   };
 
   var getDescription = function () {
-    var DESCRIPTIONS = ['описание', 'описание', 'описание', 'описание'];
+    var DESCRIPTIONS = ['Снимайте скорее', 'Вам очень понравится', 'Супер место для отдыха'];
 
     return getRandomArrayItem(DESCRIPTIONS);
   };
@@ -182,40 +182,97 @@ var getPropertyType = function (type) {
 var createCardItem = function (data) {
   var advertElement = mapCardItem.cloneNode(true);
 
-  advertElement.querySelector('.popup__title').textContent = data.offer.title;
-  advertElement.querySelector('.popup__text--address').textContent = data.offer.address;
-  advertElement.querySelector('.popup__text--price').textContent = data.offer.price + '₽/ночь';
-  advertElement.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
-  advertElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
-  advertElement.querySelector('.popup__features').textContent = data.offer.features;
-  advertElement.querySelector('.popup__description').textContent = data.offer.description;
-  advertElement.querySelector('.popup__avatar').src = data.author.avatar;
+  // Функция проверки блока на существование данных и их добавление
+  // element - путь к элементу (advertElement.querySelector('.popup__title')), info - путь к данным массива, render _функц с данными в зав от их типа
+  function createDataCardItem(element, info, render) {
+    if (info) {
+      render();
+    } else {
+      element.style.display = 'none';
+    }
+  }
+
+  var title = data.offer.title;
+  var popupTitleElement = advertElement.querySelector('.popup__title');
+  var titleRender = function () {
+    popupTitleElement.textContent = title;
+  };
+  createDataCardItem(popupTitleElement, title, titleRender);
+
+  var address = data.offer.address;
+  var popupAddressElement = advertElement.querySelector('.popup__text--address');
+  var addressRender = function () {
+    popupAddressElement.textContent = address;
+  };
+  createDataCardItem(popupAddressElement, address, addressRender);
+
+  var price = data.offer.price;
+  var popupPriceElement = advertElement.querySelector('.popup__text--price');
+  var priceRender = function () {
+    popupPriceElement.textContent = price + '₽/ночь';
+  };
+  createDataCardItem(popupPriceElement, price, priceRender);
+
+  var capacity = data.offer.rooms;
+  var popupCapacityElement = advertElement.querySelector('.popup__text--capacity');
+  var capacityRender = function () {
+    popupCapacityElement.textContent = capacity + ' комнаты для ' + data.offer.guests + ' гостей';
+  };
+  createDataCardItem(popupCapacityElement, capacity, capacityRender);
+
+  var time = data.offer.checkin;
+  var popupTimeElement = advertElement.querySelector('.popup__text--time');
+  var timeRender = function () {
+    popupTimeElement.textContent = 'Заезд после ' + time + ', выезд до ' + data.offer.checkout;
+  };
+  createDataCardItem(popupTimeElement, time, timeRender);
+
+  var features = data.offer.features;
+  var popupFeaturesElement = advertElement.querySelector('.popup__features');
+  var featuresRender = function () {
+    popupFeaturesElement.textContent = features;
+  };
+  createDataCardItem(popupFeaturesElement, features, featuresRender);
+
+  var description = data.offer.description;
+  var popupDescriptionElement = advertElement.querySelector('.popup__description');
+  var descriptionRender = function () {
+    popupDescriptionElement.textContent = description;
+  };
+  createDataCardItem(popupDescriptionElement, description, descriptionRender);
+
+  var avatar = data.author.avatar;
+  var popupAvatarElement = advertElement.querySelector('.popup__avatar');
+  var avatarRender = function () {
+    popupAvatarElement.src = avatar;
+  };
+  createDataCardItem(popupAvatarElement, avatar, avatarRender);
 
   // Отображение переведенного на RU типа жилья в карточке
-  var propertyType = getPropertyType(data.offer.type);
-  advertElement.querySelector('.popup__type').textContent = propertyType;
+  var type = data.offer.type;
+  var propertyType = getPropertyType(type);
+  var popupTypeElement = advertElement.querySelector('.popup__type');
+  var typeRender = function () {
+    popupTypeElement.textContent = propertyType;
+  };
+  createDataCardItem(popupTypeElement, propertyType, typeRender);
+  // advertElement.querySelector('.popup__type').textContent = propertyType;
 
   // Отображаем фотографии из массива photos(удаляем пустой шаблон)
+  var photos = data.offer.photos;
   var popupPhotos = advertElement.querySelector('.popup__photos');
   var popupPhotosImageTemplate = popupPhotos.querySelector('img');
   popupPhotosImageTemplate.remove();
 
-  for (var i = 0; i < data.offer.photos.length; i++) {
-    var popupPhotosImage = popupPhotosImageTemplate.cloneNode(true);
-
-    popupPhotosImage.src = data.offer.photos[i];
-    popupPhotos.appendChild(popupPhotosImage);
-  }
-
-  // У нас есть карточка эелемента (<article class="map__card popup">), нужно проверить, пусты ли дети этого Dom
-  // элемента, если да, убираем его с помощью метода el.hidden = true;
-  var advertDataElement = advertElement.children;
-  console.dir(advertDataElement);
-  for (var i = 0; i < advertDataElement.lenght; i++) {
-    if (advertDataElement[i].textContent = '') {
-      advertDataElement[i].hidden = true;
+  var photosRender = function () {
+    for (var i = 0; i < data.offer.photos.length; i++) {
+      var popupPhotosImage = popupPhotosImageTemplate.cloneNode(true);
+      popupPhotosImage.src = data.offer.photos[i];
+      popupPhotos.appendChild(popupPhotosImage);
     }
-  }
+  };
+
+  createDataCardItem(popupPhotos, photos, photosRender);
 
   return advertElement;
 };
