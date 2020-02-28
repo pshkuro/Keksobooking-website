22d86ba41ba732b2e77895e8286666d2e73c889e
Полина <polina110297@yandex.ru>
2020-02-28 13:52:04 +0300

    Б13

--- js/util.js
+++ js/util.js
@@ -1,25 +1,6 @@
 'use strict';
 
 (function () {
-
-  // Функция генерации случ числа от min до max
-  function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  // Функция генерации случ эл заданного массива
-  function getRandomArrayItem(arr) {
-    var index = Math.floor(Math.random() * arr.length);
-    return arr[index];
-  }
-
-  // Функция получения массива случайной длины
-  function getArrayRandomLength(arr) {
-    var randomLength = getRandomInRange(1, arr.length);
-
-    return arr.slice(0, randomLength);
-  }
-
   // Функция Блокировки и Разблокировки полей формы
   function setDisabledFormFields(form, disabled) {
     var formChildren = form.children;
@@ -29,9 +10,6 @@
   }
 
   window.util = {
-    getRandomInRange: getRandomInRange,
-    getRandomArrayItem: getRandomArrayItem,
-    getArrayRandomLength: getArrayRandomLength,
     setDisabledFormFields: setDisabledFormFields
   };
 })();
